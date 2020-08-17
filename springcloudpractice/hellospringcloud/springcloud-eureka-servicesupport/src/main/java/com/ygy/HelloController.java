c513544f68eca21bc64b4f078f52e6c25874ba24
yanggy-a <840096642@qq.com>
2020-08-17 17:47:52 +0800

    练习

--- springcloudpractice/hellospringcloud/springcloud-eureka-servicesupport/src/main/java/com/ygy/HelloController.java
+++ springcloudpractice/hellospringcloud/springcloud-eureka-servicesupport/src/main/java/com/ygy/HelloController.java
@@ -1,11 +1,22 @@
 package com.ygy;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
     /**
      * 输入用户名字name，返回hello name
      * @param name
